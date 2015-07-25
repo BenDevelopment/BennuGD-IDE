0b92f4bcc517bc88de0be743278b18eb50e27173
unknown <bhoffmann68@gmail.com>
2015-07-25 14:14:44 +0200

    Ajout projet

---
+++ ide/plugins/NppExec/BaseDef.h
@@ -0,0 +1,8 @@
+#define WM_COMMAND     0x0111
+#define WM_SYSCOMMAND  0x0112
+#define WM_USER        0x0400
+#define NPPMSG         (WM_USER + 1000)
+
+// add more definitions if needed...
+#define pi 3.14159265359
+#define e  2.718281828459
