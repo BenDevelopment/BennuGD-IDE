2e8d896083d4c7df11a62baf1e500cfedc804b6b
Ben Otoktone <bhoffmann68@gmail.com>
2016-01-03 14:35:55 +0100

    Ameliorations

--- bin.back/describe.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo BENNU FUNCTIONS > function-list.txt
-echo --------------- >> function-list.txt
-for %%i in (lib*.dll) do echo %%i && moddesc -i- %%i >> function-list.txt && echo ------------------------------------------------------- >> function-list.txt
-for %%i in (mod*.dll) do echo %%i && moddesc -i- %%i >> function-list.txt && echo ------------------------------------------------------- >> function-list.txt
-
